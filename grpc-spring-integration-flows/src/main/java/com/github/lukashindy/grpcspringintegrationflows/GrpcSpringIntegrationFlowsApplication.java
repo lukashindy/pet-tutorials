dea37091d9a4a58b731b99953aa244976505e26d
nmescv <mescvnv@gmail.com>
2022-07-21 21:38:24 +0300

    Initial commit

---
+++ grpc-spring-integration-flows/src/main/java/com/github/lukashindy/grpcspringintegrationflows/GrpcSpringIntegrationFlowsApplication.java
@@ -0,0 +1,13 @@
+package com.github.lukashindy.grpcspringintegrationflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringIntegrationFlowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringIntegrationFlowsApplication.class, args);
+	}
+
+}
---
+++ spring-testcontainers/src/test/java/com/github/lukashindy/springtestcontainers/SpringTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lukashindy.springtestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
